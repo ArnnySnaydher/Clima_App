291f554531848c1d24ca113d628ceb89aed712ca
Aryans <quispeloayza18@hotmail.com>
2024-03-07 11:03:18 -0500

    Validando formulario

---
+++ src/components/Alerta.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="alerta">
+        <slot></slot>
+    </div>
+</template>
+
+<style>
+    .alerta {
+        text-align: center;
+        margin-bottom: 2rem;
+        /* color: var(--blanco); */
+        color:#CA8A04;
+        text-transform: uppercase;
+        font-weight: 900;
+        font-size: 1.2rem;
+        padding: .5rem
+    }
+</style>
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
+import Alerta from './Alerta.vue';
+import Swal from 'sweetalert2'
 
 const busqueda = reactive({
-    ciudad:'',
-    pais:''
+    ciudad: '',
+    pais: ''
 })
+
+const error = ref("")
 type Pais = {
-    codigo:string;
-    nombre:string;
+    codigo: string;
+    nombre: string;
 };
 
-const paises:Pais[]= [
+const paises: Pais[] = [
     { codigo: 'US', nombre: 'Estados Unidos' },
     { codigo: 'MX', nombre: 'México' },
     { codigo: 'AR', nombre: 'Argentina' },
@@ -19,38 +23,47 @@ const paises:Pais[]= [
     { codigo: 'ES', nombre: 'España' },
     { codigo: 'PE', nombre: 'Perú' }
 ]
+
+const consultarClima = () => {
+
+    if (Object.values(busqueda).includes('')) {
+        Swal.fire("Todos  los campos son obligatorios");
+        return
+        // error.value = "Todos  los campos son obligatorios"
+
+        // setTimeout(() => {
+        //     error.value = ''
+        // }, 2000);
+        // return
+    }
+
+
+}
 </script>
 <template>
-    <form class="formulario flex flex-col gap-5" >
+
+    <form @submit.prevent="consultarClima" class="formulario flex flex-col gap-5">
+
+        <Alerta v-if="error">{{ error }}</Alerta>
         <div class="campo flex flex-col gap-5">
-            <label 
-            class="text-xl font-bold"
-            for="ciudad">Ciudad</label>
-            <input 
-            v-model="busqueda.ciudad" 
-            class="p-3 bg-transparent border border-solid border-white rounded-lg text-white font-normal text-2xl "
-            type="text"
-            id="ciudad"
-            placeholder="Ciudad">
+            <label class="text-xl font-bold" for="ciudad">Ciudad</label>
+            <input v-model="busqueda.ciudad"
+                class="p-3 bg-transparent border border-solid border-white rounded-lg text-white font-normal text-2xl "
+                type="text" id="ciudad" placeholder="Ciudad">
         </div>
 
         <div class="campo flex flex-col gap-5">
-            <label 
-            class="text-xl font-bold"
-            for="pais">Pais</label>
-            <select
-            v-model="busqueda.pais"
-            class="p-3 bg-transparent border border-solid border-white rounded-lg text-white font-normal text-lg "
-            name="" 
-            id="pais">
-            <option class="text-black" value="">--Selecione un Pais--</option>
-            <option  class="text-black" v-for="pais in paises" :value="pais.codigo">{{ pais.nombre }}</option>
+            <label class="text-xl font-bold" for="pais">Pais</label>
+            <select v-model="busqueda.pais"
+                class="p-3 bg-transparent border border-solid border-white rounded-lg text-white font-normal text-lg "
+                name="" id="pais">
+                <option class="text-black" value="">--Selecione un Pais--</option>
+                <option class="text-black" v-for="pais in paises" :value="pais.codigo">{{ pais.nombre }}</option>
             </select>
         </div>
 
-        <input 
-        class="bg-yellow-600 border-none text-white font-bold text-sm uppercase cursor-pointer p-2.5 mt-5  "
-        type="submit" value="Consultar Clima">
+        <input class="bg-indigo-500 border-none text-white font-bold text-sm uppercase cursor-pointer p-2.5 mt-5  "
+            type="submit" value="Consultar Clima">
     </form>
 </template>
 
@@ -60,6 +73,3 @@ const paises:Pais[]= [
     opacity: 1;
 } */
 </style>
-
-
-

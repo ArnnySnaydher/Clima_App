c3ee2e18d172601542ac875be74b017eedc3a3e4
Aryans <quispeloayza18@hotmail.com>
2024-03-07 10:00:54 -0500

    add estilos

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,13 @@ import Formulario from './components/Formulario.vue'
 </script>
 
 <template>
-  
-  <body class="bg-image h-screen bg-no-repeat bg-cover font-bold text-white text-xl" >
-    <h1 class="titulo text-center text-3xl mt-12">Buscador de Clima</h1>
-    <div class="contenedor buscador-clima">
+
+  <div class="bg-cover bg-no-repeat bg-center bg-fixed h-screen font-outfit text-white text-2xl bg-image">
+    <!-- Contenido de tu componente Vue -->
+    <h1 class="titulo text-center text-3xl pt-12">Buscador de Clima</h1>
+    <div class="contenedor buscador-clima grid grid-cols-2 items-center gap-10 mt-12 mx-auto w-11/12 max-w-5xl">
       <Formulario></Formulario>
     </div>
-  </body>
+  </div>
   
 </template>
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -21,7 +21,7 @@ const paises:Pais[]= [
             class="text-xl font-bold"
             for="ciudad">Ciudad</label>
             <input 
-            class="p-4 bg-transparent border border-solid border-white rounded-lg text-white font-normal text-2xl "
+            class="p-3 bg-transparent border border-solid border-white rounded-lg text-white font-normal text-2xl "
             type="text"
             id="ciudad"
             placeholder="Ciudad">
@@ -32,15 +32,22 @@ const paises:Pais[]= [
             class="text-xl font-bold"
             for="ciudad">Pais</label>
             <select
-            class="p-4 bg-transparent border border-solid border-white rounded-lg text-white font-normal text-2xl "
+            class="p-3 bg-transparent border border-solid border-white rounded-lg text-white font-normal text-lg "
             name="" 
             id="">
-            <option value="">--Selecione un Pais</option>
-            <option v-for="pais in paises" :value="pais.codigo">{{ pais.nombre }}</option>
+            <option class="text-black" value="">--Selecione un Pais--</option>
+            <option  class="text-black" v-for="pais in paises" :value="pais.codigo">{{ pais.nombre }}</option>
             </select>
         </div>
     </form>
 </template>
+
+<style scoped>
+.campo::placeholder{
+    color: white;
+    opacity: 1;
+}
+</style>
 
 
 

49f42815b9280fe6a08e9649770a2b4bee105541
Aryans <quispeloayza18@hotmail.com>
2024-03-06 23:51:36 -0500

    change estilos

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
-<script lang="ts">
+<script setup lang="ts">
+import Formulario from './components/Formulario.vue'
 </script>
 
 <template>
-  <body class="bg-image h-screen bg-no-repeat bg-cover font-sans text-white text-sm" >
-    
+  
+  <body class="bg-image h-screen bg-no-repeat bg-cover font-bold text-white text-xl" >
+    <h1 class="titulo text-center text-3xl mt-12">Buscador de Clima</h1>
+    <div class="contenedor buscador-clima">
+      <Formulario></Formulario>
+    </div>
   </body>
+  
 </template>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+type Pais = {
+    codigo:string;
+    nombre:string;
+};
+
+const paises:Pais[]= [
+    { codigo: 'US', nombre: 'Estados Unidos' },
+    { codigo: 'MX', nombre: 'México' },
+    { codigo: 'AR', nombre: 'Argentina' },
+    { codigo: 'CO', nombre: 'Colombia' },
+    { codigo: 'CR', nombre: 'Costa Rica' },
+    { codigo: 'ES', nombre: 'España' },
+    { codigo: 'PE', nombre: 'Perú' }
+]
+</script>
+<template>
+    <form class="formulario flex flex-col gap-5" >
+        <div class="campo flex flex-col gap-5">
+            <label 
+            class="text-xl font-bold"
+            for="ciudad">Ciudad</label>
+            <input 
+            class="p-4 bg-transparent border border-solid border-white rounded-lg text-white font-normal text-2xl "
+            type="text"
+            id="ciudad"
+            placeholder="Ciudad">
+        </div>
+
+        <div class="campo flex flex-col gap-5">
+            <label 
+            class="text-xl font-bold"
+            for="ciudad">Pais</label>
+            <select
+            class="p-4 bg-transparent border border-solid border-white rounded-lg text-white font-normal text-2xl "
+            name="" 
+            id="">
+            <option value="">--Selecione un Pais</option>
+            <option v-for="pais in paises" :value="pais.codigo">{{ pais.nombre }}</option>
+            </select>
+        </div>
+    </form>
+</template>
+
+
+

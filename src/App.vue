01dd9135d68566a7ddb1093997d751a6634259d2
Aryans <quispeloayza18@hotmail.com>
2024-03-06 23:04:23 -0500

    First commint

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<template>
+  <body class="bg-image h-screen bg-no-repeat bg-cover font-sans text-white text-sm" >
+    
+  </body>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    backgroundImage: {
+      'image':"url(public/img/bg_clima.jpg)"
+    },
+  },
+  plugins: [],
+}
+
